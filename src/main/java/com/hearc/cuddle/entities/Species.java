7458d59b0feef601fc444c52294a3e5dd0e0bed9
Valentin Wyss <valentin.wyss@hes-so.ch>
2021-03-15 10:48:07 +0100

    Started creating models

---
+++ src/main/java/com/hearc/cuddle/entities/Species.java
@@ -0,0 +1,31 @@
+package com.hearc.cuddle.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="species")
+public class Species {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/com/hearc/cuddle/entities/User.java
+++ src/main/java/com/hearc/cuddle/entities/User.java
@@ -5,13 +5,26 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class User {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
 
     public void setId(Long id) {
         this.id = id;
@@ -22,4 +35,35 @@ public class User {
         return id;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
