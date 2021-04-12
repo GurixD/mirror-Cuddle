41d196d65b6daac168f7de294f8664bc8cf3b72c
Florian <florian.feuillade@he-arc.ch>
2021-04-12 10:37:49 +0200

    update navbar and design

--- src/main/java/com/hearc/cuddle/auth/WebSecurityConfig.java
+++ src/main/java/com/hearc/cuddle/auth/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 
 @Configuration
@@ -36,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll()
                 .and()
-                .logout()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .permitAll();
 //        http
 //                .formLogin()
