8f0df5bae63250c3bb5e440ebc23e7ec460d7cdf
Valentin Wyss <valentin.wyss@hes-so.ch>
2021-04-19 13:00:19 +0200

    Add new animal form, WIP

--- src/main/java/ch/hearc/cuddle/controllers/DashboardController.java
+++ src/main/java/ch/hearc/cuddle/controllers/DashboardController.java
@@ -1,19 +1,50 @@
 package ch.hearc.cuddle.controllers;
 
+import ch.hearc.cuddle.models.Animal;
+import ch.hearc.cuddle.models.Breed;
+import ch.hearc.cuddle.models.Species;
+import ch.hearc.cuddle.service.BreedService;
+import ch.hearc.cuddle.service.SpeciesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
-    @GetMapping("/addAnimal")
-    public String addAnimal(Model model)
-    {
+    @Autowired
+    SpeciesService speciesService;
 
+    @Autowired
+    BreedService breedService;
+
+
+    @GetMapping("/addAnimal")
+    public String addAnimal(Model model) {
+        List<Species> species = speciesService.findAll();
+        List<Breed> breeds = breedService.findAll();
+        Animal newAnimal = new Animal();
+
+        model.addAttribute("species", species);
+        model.addAttribute("breeds", breeds);
+        model.addAttribute("newAnimal", newAnimal);
 
         return "addAnimal";
+    }
+
+    @PostMapping("/addAnimal")
+    public String addAnimal(@ModelAttribute("newAnimal") Animal newAnimal) {
+
+        System.out.println(newAnimal.getName());
+
+        return "redirect:/home";
     }
 }
