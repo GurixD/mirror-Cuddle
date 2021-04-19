e0ae8fee72b9b03362915e61c6b230f5e2e1ea4b
Valentin Wyss <valentin.wyss@hes-so.ch>
2021-04-19 09:39:43 +0200

    Replace alpine-docker with Cuddle runner

--- src/test/java/ch/hearc/cuddle/CuddleApplicationTests.java
+++ src/test/java/ch/hearc/cuddle/CuddleApplicationTests.java
@@ -1,5 +1,6 @@
 package ch.hearc.cuddle;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,6 @@ class CuddleApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
-
 }
