9e84eff255dd6f06eae68041e44309b75fb86c4c
Antoine <antoine.lestrade89@gmail.com>
2021-04-19 10:37:24 +0200

    Fix load CSS and JS when anonymous

--- src/main/java/ch/hearc/cuddle/auth/WebSecurityConfig.java
+++ src/main/java/ch/hearc/cuddle/auth/WebSecurityConfig.java
@@ -29,7 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/resources/**", "/registration", "/", "/home").permitAll()
+                .antMatchers("/resources/**", "/css/**", "/js/**" ,
+                        "/registration", "/", "/home").permitAll()
                 .antMatchers("/*").hasAnyRole("ADMIN","STAFF","VETENARY")
                 .anyRequest().authenticated()
                 .and()
