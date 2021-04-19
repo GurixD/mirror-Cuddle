86750a5f28158a94a6a2a0bc37056df513e941cb
Antoine <antoine.lestrade89@gmail.com>
2021-04-19 12:49:10 +0200

    Adopt page + UI improvements

---
+++ src/main/java/ch/hearc/cuddle/controllers/AdoptController.java
@@ -0,0 +1,33 @@
+package ch.hearc.cuddle.controllers;
+
+import ch.hearc.cuddle.auth.repository.UserRepository;
+import ch.hearc.cuddle.models.Animal;
+import ch.hearc.cuddle.service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class AdoptController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AnimalService animalService;
+
+    @GetMapping({"/adopt"})
+    public String adopt(Model model) {
+        List<Animal> animalsList = animalService.listAll();
+        System.out.println(animalsList);
+//        model.addAttribute("name", name);
+        model.addAttribute("listAnimals", animalsList);
+        return "adopt";
+    }
+
+
+}
