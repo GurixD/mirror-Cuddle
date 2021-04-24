8040a5add35907e2564039f563381f69d431bf9d
Antoine <antoine.lestrade89@gmail.com>
2021-04-24 21:24:55 +0200

    Handle max file size exception

---
+++ src/main/java/ch/hearc/cuddle/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package ch.hearc.cuddle;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+        System.out.println("YO2? : "+e.getCause().getMessage());
+//        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        return "dashboard";
+
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxUploadSizeError(MaxUploadSizeExceededException e, RedirectAttributes redirectAttributes) {
+        System.out.println("YO? : "+e.getCause().getMessage());
+//        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        return "dashboard";
+
+    }
+}
+
