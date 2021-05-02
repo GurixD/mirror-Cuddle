d4f671ca2e3d4d7e479f42daff8829d998f19324
valentin.wyss <valentin.wyss@he-arc.ch>
2021-05-02 14:03:56 +0000

    Image ok docker

--- src/main/java/ch/hearc/cuddle/MvcConfig.java
+++ src/main/java/ch/hearc/cuddle/MvcConfig.java
@@ -1,32 +1,27 @@
 package ch.hearc.cuddle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/media/**").addResourceLocations("file:///" + System.getProperty("user.dir") + "/src/main/media/");
+        
+
+        registry.addResourceHandler("/media/**").addResourceLocations("file:///app/static/media/");
     }
-
-
-    // @Override
-    // public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    //     exposeDirectory("media/img", registry);
-    // }
-
-    // private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-    //     Path uploadDir = Paths.get(dirName);
-    //     String uploadPath = uploadDir.toFile().getAbsolutePath();
-
-    //     if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-
-    //     registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
-    // }
 }
